fd537bdbca8a8225d72da070d9839c788f13e018
Caballo-loko <thras3r88@gmail.com>
2024-07-27 13:53:43 -0700

    Initial commit for Casino of Life

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -16,9 +16,9 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by editing <code>app/page.tsx</code>
+            The beginning of the Casino of life <code>app/page.tsx</code>
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Bet your Sol for FreeDUMBS on the machine in Mortal Kombat.</li>
         </ol>
 
         <div className={styles.ctas}>
@@ -35,7 +35,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            Deposit your Sol now
           </a>
           <a
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
@@ -64,20 +64,6 @@ export default function Home() {
             height={16}
           />
           Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
         </a>
         <a
           href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
